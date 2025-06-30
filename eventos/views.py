57949953554d2e8ce6412d014e90789658c7327b
Kauã <skaua001@gmail.com>
2025-06-30 11:59:31 -0300

    Autenticação nas rotas

--- eventos/views.py
+++ eventos/views.py
@@ -3,11 +3,12 @@ from .models import Evento
 from .forms import EventoForm
 from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def listar_eventos(request):
     eventos = Evento.objects.all().order_by('data')
     return render(request, 'eventos/listar_eventos.html', {'eventos': eventos})
 
-@login_required
+@login_required(login_url='login')
 def criar_evento(request):
     if request.method == 'POST':
         form = EventoForm(request.POST)
@@ -20,7 +21,7 @@ def criar_evento(request):
         form = EventoForm()
     return render(request, 'eventos/criar_evento.html', {'form': form})
 
-
+@login_required(login_url='login')
 def sobre(request):
     return render(request, 'eventos/sobre.html')
 
